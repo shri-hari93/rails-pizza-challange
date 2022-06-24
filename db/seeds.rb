cec5878ad9d52c0dc48ff5cd28766d5a68c68574
shri-hari93 <shrihari.g93@gmail.com>
2022-06-24 13:20:35 +0200

    Seeding only for development environment
    Signed-off-by: shri-hari93 <shrihari.g93@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-order = Order.new
-order.items.build(name: 'Tonno', size: 'Large')
-order.save!
 
-order = Order.new
-order.items.build(name: 'Margherita', size: 'Large', add: ['Onions', 'Cheese', 'Olives'])
-order.items.build(name: 'Tonno', size: 'Medium', remove: ['Onions', 'Olives'])
-order.items.build(name: 'Margherita', size: 'Small')
-order.save!
+case Rails.env
+when "development"
+  order = Order.new
+  order.items.build(name: 'Tonno', size: 'Large')
+  order.save!
 
-order = Order.new(promotion_code: ['2FOR1'], discount_code: 'SAVE5')
-order.items.build(name: 'Salami', size: 'Medium', add: ['Onions'], remove: ['Cheese'])
-order.items.build(name: 'Salami', size: 'Small')
-order.items.build(name: 'Salami', size: 'Small')
-order.items.build(name: 'Salami', size: 'Small')
-order.items.build(name: 'Salami', size: 'Small', add: ['Olives'])
-order.save!
+  order = Order.new
+  order.items.build(name: 'Margherita', size: 'Large', add: ['Onions', 'Cheese', 'Olives'])
+  order.items.build(name: 'Tonno', size: 'Medium', remove: ['Onions', 'Olives'])
+  order.items.build(name: 'Margherita', size: 'Small')
+  order.save!
+
+  order = Order.new(promotion_code: ['2FOR1'], discount_code: 'SAVE5')
+  order.items.build(name: 'Salami', size: 'Medium', add: ['Onions'], remove: ['Cheese'])
+  order.items.build(name: 'Salami', size: 'Small')
+  order.items.build(name: 'Salami', size: 'Small')
+  order.items.build(name: 'Salami', size: 'Small')
+  order.items.build(name: 'Salami', size: 'Small', add: ['Olives'])
+  order.save!
+end
